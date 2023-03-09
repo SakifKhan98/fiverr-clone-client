32e504d6f0c01229ccdc6092f6a58605bebab56d
SakifKhan98 <sakifkhan98@gmail.com>
2023-03-10 02:13:19 +0600

    Added navbar design

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,50 @@
-function App() {
-  return <div>Hello world!</div>
+import React from 'react'
+import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/footer'
+import Home from './pages/home/Home'
+import Gigs from './pages/gigs/Gigs'
+import Gig from './pages/gig/Gig'
+import Orders from './pages/orders/Orders'
+import MyGigs from './pages/myGigs/MyGigs'
+import Add from './pages/add/Add'
+import Messages from './pages/messages/Messages'
+import Message from './pages/message/Message'
+import './app.scss'
+
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+
+const App = () => {
+  const Layout = () => {
+    return (
+      <div className="app">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    )
+  }
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        { path: '/', element: <Home /> },
+        { path: '/gigs', element: <Gigs /> },
+        { path: '/gig/:id', element: <Gig /> },
+        { path: '/orders', element: <Orders /> },
+        { path: '/mygigs', element: <MyGigs /> },
+        { path: '/add', element: <Add /> },
+        { path: '/messages', element: <Messages /> },
+        { path: '/message/:id', element: <Message /> },
+      ],
+    },
+  ])
+  return (
+    <div>
+      <RouterProvider router={router} />
+      {/* <Navbar /> */}
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Footer.scss'
+
+const Footer = () => {
+  return <div>THIS IS THE FOOTER</div>
+}
+
+export default Footer
